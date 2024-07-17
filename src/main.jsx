e860927068d96a4085199b2809f19a6f8082546f
Anuj Chaudhary <alwaysmile9813@gmail.com>
2024-07-18 00:18:06 +0530

    routing setup

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,16 @@ import './index.css'
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
+import Portfolio from './components/Portfolio/Portfolio.jsx'
+import Contact from './components/Contact/Contact.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
       <Route path='/' element={<Home />} />
       <Route path='/about' element={<About />} />
-      {/* <Route path='/' element={<Home />} /> */}
+      <Route path='/portfolio' element={<Portfolio />} />
+      <Route path='/contact' element={<Contact />} />
     </Route>
   )
 )
