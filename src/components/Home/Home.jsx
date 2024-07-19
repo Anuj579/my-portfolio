de058f477e7512ebf8e174d880409ec78782ba1a
Anuj Chaudhary <alwaysmile9813@gmail.com>
2024-07-19 23:01:13 +0530

    updates

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -203,10 +203,10 @@ function Home({ isLoading }) {
         </div>
 
         {/* Quote div */}
-        <div className='py-20 flex flex-col items-center'>
+        <div className='py-20 text-center flex flex-col items-center'>
           <h1 className='font-cairo text-3xl md:text-[40px] font-semibold'>“If, at first, you do not succeed, call it version 1.0”.</h1>
           <p className='mt-6 text-[20px] text-accent'>- Khayri R.R. Woulfe</p>
-          <img src={zigzagLine} alt="zigzag-line" className='w-80 mt-5 -skew-x-[20deg]' />
+          <img src={zigzagLine} alt="zigzag-line" className='w-56 md:w-80 mt-10 -skew-x-[20deg]' />
         </div>
 
         {/* Let's Connect div */}
