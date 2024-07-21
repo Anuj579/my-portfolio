f39d5ffb40ef029ebed3abc984eade291debf0d6
Anuj Chaudhary <alwaysmile9813@gmail.com>
2024-07-21 21:17:34 +0530

    Load more link enhanced

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -18,6 +18,7 @@ import { Breadcrumbs } from "@material-tailwind/react";
 import { Link } from "react-router-dom";
 import ProjectCard from './ProjectCard';
 import Connect from '../Connect'
+import './Portfolio.css'
 
 function Portfolio() {
   const [showAll, setShowAll] = useState(false);
@@ -115,9 +116,9 @@ function Portfolio() {
           <div className='flex justify-center'>
             <button
               onClick={() => setShowAll(!showAll)}
-              className=' text-white hover:underline'
+              className=' text-white load-link flex items-center font-semibold'
             >
-              {showAll ? 'Show Less...' : 'Load More...'}
+              <span>{showAll ? 'Show Less' : 'Load More'}</span><svg xmlns="http://www.w3.org/2000/svg" className='h-[16px] fill-white ms-3' viewBox="0 0 512 512"><path d="M502.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l370.7 0-73.4 73.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l128-128z" /></svg>
             </button>
           </div>
         </div>
