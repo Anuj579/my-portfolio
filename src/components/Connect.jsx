9da57182405fd3bf7eaee03e7e05615eda1a72a0
Anuj Chaudhary <alwaysmile9813@gmail.com>
2024-07-30 22:57:18 +0530

    styling updates, shadow added

--- src/components/Connect.jsx
+++ src/components/Connect.jsx
@@ -6,7 +6,7 @@ import arrow from '../assets/arrow.svg'
 
 function Connect() {
     return (
-        <div className='rounded-xl py-12 md:py-16 px-2 flex flex-col items-center shadow-xl  bg-gradient-to-tr from-primary to-[#010b138a] shadow-[#00000057] relative mb-14' >
+        <div className='rounded-xl py-12 md:py-16 px-2 flex flex-col items-center shadow-lg  bg-gradient-to-tr from-primary to-[#010b139a] shadow-[#44adf813] relative mb-14' >
             <img src={triangle} alt="triangle" className='h-4 md:h-5 absolute left-[10%] top-[20%] animate-rotate-up-down-left' />
             <img src={triangle} alt="triangle" className='h-4 md:h-5 absolute right-[10%] top-[30%] rotate-[78deg] animate-rotate-up-down-right' />
             <img src={triangle} alt="triangle" className='h-4 md:h-5 absolute left-2/4 bottom-[5%] rotate-[-90deg] animate-rotate-left-right' />
