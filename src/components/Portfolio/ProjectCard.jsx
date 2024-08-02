ef2f555e917bfdc87b0e4c090eafa4443042bb81
Anuj Chaudhary <alwaysmile9813@gmail.com>
2024-08-03 00:36:57 +0530

    useless classes removed

--- src/components/Portfolio/ProjectCard.jsx
+++ src/components/Portfolio/ProjectCard.jsx
@@ -9,8 +9,8 @@ function ProjectCard({ laptopImg, mobileImg, title, description, demoLink, codeL
         <div className='rounded-md relative pt-8 pb-16 md:pb-10 mt-14 w-[340px] bg-[#010b139f] border border-[#ffffff25] md:shadow-custom-card'>
             <img src={gradient} alt="gradient" className={`absolute -top-28 w-96 ${className}`} />
             <div className='relative -top-16'>
-                <img src={laptopImg} alt="project1" className='object-cover object-center' loading='lazy' />
-                <img src={mobileImg} alt="project1" className='h-44 absolute object-cover object-center top-10 right-0' loading='lazy' />
+                <img src={laptopImg} alt="project1"/>
+                <img src={mobileImg} alt="project1" className='h-44 absolute  top-10 right-0'/>
             </div>
             <div className='text-center mx-5 flex flex-col items-center'>
                 <h2 className='text-2xl mb-4 font-semibold'>{title}</h2>
